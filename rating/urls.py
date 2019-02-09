c7113f301c01d659b5f23dfd247b5d55741c4fce
garvitkataria <garvit.k16@iiits.in>
2019-02-09 09:49:26 +0530

    completed APIs for the 3 Algos of RS

--- rating/urls.py
+++ rating/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url,include
 from . import views
 from rest_framework import routers
-from .views import RecommendView
+from .views import RecommendView,Recommend2View,Recommend3View
 
 router = routers.DefaultRouter()
 router.register('', views.ratingView)
@@ -9,6 +9,8 @@ router.register('', views.ratingView)
 urlpatterns = [
 
 	url(r'^recommend/$', RecommendView.as_view()),
+	url(r'^recommend2/$', Recommend2View.as_view()),
+	url(r'^recommend3/$', Recommend3View.as_view()),
 	url(r'', include(router.urls)),
 	
 ]
--- rating/views.py
+++ rating/views.py
@@ -7,6 +7,9 @@ from .serializers import ratingSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import pandas as pd
+import numpy as np
+from scipy.sparse.linalg import svds
+
 # Create your views here.
 class ratingView(viewsets.ModelViewSet):
 	queryset = Rating.objects.all()
@@ -14,9 +17,11 @@ class ratingView(viewsets.ModelViewSet):
 
 
 class RecommendView(APIView):
-
+	
 	def get(self, request, format=None):
 		# print("HI")
+		# user_id="4"
+		user_id= request.GET.get('user_id')
 		rating_r = requests.get('http://127.0.0.1:8000/rating/')
 		rating_data=rating_r.json()
 		rating = pd.DataFrame.from_dict(rating_data)
@@ -26,7 +31,7 @@ class RecommendView(APIView):
 		corrMatrix = userRatings.corr()
 		# print(corrMatrix.head())
 
-		myRatings = userRatings.loc["http://127.0.0.1:8000/user/1/"].dropna()
+		myRatings = userRatings.loc["http://127.0.0.1:8000/user/"+user_id+"/"].dropna()
 
 		simCandidates = pd.Series()
 		for i in range(0, len(myRatings.index)):
@@ -36,9 +41,113 @@ class RecommendView(APIView):
 
 		simCandidates = simCandidates.groupby(simCandidates.index).sum()
 		simCandidates.sort_values(inplace = True, ascending = False)
-		print (simCandidates.head())
-		df1 = pd.DataFrame(data=simCandidates.index, columns=['movie'])
-		df2 = pd.DataFrame(data=simCandidates.values, columns=['val'])
-		df = pd.merge(df1, df2, left_index=True, right_index=True)
-		return Response(df)
+		# print(simCandidates)
+
+		simCandidates_positives=simCandidates
+		simCandidates_positives
+
+		List_rec_movies=[]
+		for i in range(len(simCandidates_positives)):
+		    temp=simCandidates_positives.index[i]
+		    List_rec_movies.append(str(temp).split('/')[-2])
+		List_rec_movies
+
+		List_i_rec=[]
+		for i in range(len(myRatings)):
+		    temp=myRatings.index[i]
+		    # print(temp)
+		    List_i_rec.append(str(temp).split('/')[-2])
+		List_i_rec
+
+		Final_list=[]
+		for i in List_rec_movies:
+		    if i in List_i_rec:
+		        pass
+		    else:
+		        Final_list.append(i)
+		# print(Final_list)
+
+		Final_list_top_ten=Final_list[0:10]
+		return Response(Final_list_top_ten)
+
+class Recommend2View(APIView):
+
+	def get(self, request, format=None):
+		# user_id="1"
+		user_id= request.GET.get('user_id')
+		rating_r = requests.get('http://127.0.0.1:8000/rating/')
+		rating_data=rating_r.json()
+
+		rating = pd.DataFrame.from_dict(rating_data)
+		userRatings = rating.pivot_table(index=['movie'],columns=['user'],values='rating')
+		corrMatrix = userRatings.corr()
+		myRatings = userRatings.loc[:,"http://127.0.0.1:8000/user/"+user_id+"/"].dropna()
+		my_releation_with_users = corrMatrix.loc[:,"http://127.0.0.1:8000/user/"+user_id+"/"].dropna()
+		my_releation_with_users_positive=my_releation_with_users[my_releation_with_users>0]
+		my_releation_with_users_positive_2 = my_releation_with_users_positive.drop("http://127.0.0.1:8000/user/"+user_id+"/")
+		simCandidates = pd.Series()
+
+		for i in range(len(my_releation_with_users_positive_2)):
+		    sim = userRatings[my_releation_with_users_positive_2.index[i]].dropna()
+		    sim = sim.map(lambda x: x * (my_releation_with_users_positive_2[i]))
+		    simCandidates = simCandidates.append(sim)
+		simCandidates = simCandidates.groupby(simCandidates.index).sum()
+		simCandidates.sort_values(inplace = True, ascending = False)
+		simCandidates_positives=simCandidates[simCandidates>0.5]
+		List_rec_movies=[]
+		for i in range(len(simCandidates_positives)):
+		    temp=simCandidates_positives.index[i]
+		    List_rec_movies.append(str(temp).split('/')[-2])
+		List_i_rec=[]
+		for i in range(len(myRatings)):
+		    temp=myRatings.index[i]
+		    List_i_rec.append(str(temp).split('/')[-2])
+		Final_list=[]
+		for i in List_rec_movies:
+		    if not i in List_i_rec:
+		        Final_list.append(i)
+		Final_list_top_ten=Final_list[0:10]
+		return Response(Final_list_top_ten)
+
+class Recommend3View(APIView):
+	def get(self, request, format=None):
+		user_id= request.GET.get('user_id')
+
+		rating_r = requests.get('http://127.0.0.1:8000/rating/')
+		rating_data=rating_r.json()
+
+		rating = pd.DataFrame.from_dict(rating_data)
+		userRatings = rating.pivot_table(index=['user'],columns=['movie'],values='rating').fillna(0)
+
+		R = userRatings.as_matrix()
+		user_ratings_mean = np.mean(R, axis = 1)
+		R_demeaned = R - user_ratings_mean.reshape(-1, 1)
+
+		
+		U, sigma, Vt = svds(R_demeaned, k = 7)
+		sigma = np.diag(sigma)
+		all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
+
+		myRatings = userRatings.loc["http://127.0.0.1:8000/user/"+user_id+"/"]
+		myRatings = myRatings[myRatings>0]
+
+		List_i_rec=[]
+		for i in range(len(myRatings)):
+		    temp=myRatings.index[i]
+		    print(temp)
+		    List_i_rec.append(str(temp).split('/')[-2])
+
+		preds_df = pd.DataFrame(all_user_predicted_ratings, columns = userRatings.columns,index= userRatings.index)
+		preds_df_user = preds_df.loc['http://127.0.0.1:8000/user/'+user_id+'/']
+		preds_df_user.sort_values(inplace = True, ascending = False)
+
+		Final_list=[]
+		for i in preds_df_user.index:
+		    if i.split('/')[-2] not in List_i_rec:
+		        Final_list.append(i.split('/')[-2])
+		Final_list_top_ten=Final_list[0:10]
+		return Response(Final_list_top_ten)
+
+
+
 
